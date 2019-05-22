3124a7d1b49c8e130567330640f7913878f94548
dodicin <nostalgia.driver@gmail.com>
2019-05-22 10:13:32 +0200

    added metrics and log writing

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,28 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 #include <Eigen/Sparse>
 #include <Eigen/unsupported/SparseExtra>
 
 using namespace Eigen;
 
-typedef SparseMatrix<double> SpMat;
+typedef SparseMatrix<double, ColMajor, int64_t> SpMat;
 
-int main()
+std::string get_platform()
 {
+    #ifdef _WIN32
+    return "Windows";
+    #elif _WIN64
+    return "Windows";
+    #elif __unix || __unix__
+    return "Unix";
+    #elif __APPLE__ || __MACH__
+    return "MacOSX";
+    #elif __linux__
+    return "Linux";
+    #elif __FreeBSD__
+    return "FreeBSD";
+    #else
+    return "Other";
+    #endif
+} 
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2) return -1;
+
+    std::string mat_name = argv[1];
+    std::string res_path = "../res/matrices/";
+    std::string mat_path = res_path + mat_name;
+
     SpMat A;
-    loadMarket(A, "../res/matrices/ex15.mtx");
+    loadMarket(A, mat_path);
 
     int n = A.cols();
     VectorXd xe = VectorXd::Constant(n, 1);
-    MatrixXd b = A.selfadjointView<Upper>() * xe;
+    MatrixXd b = A.selfadjointView<Lower>() * xe;
     //MatrixXd b = A * xe;
 
     // Cholesky Factorization:
-    SimplicialCholesky<SpMat> chol(A);
+    auto start = std::chrono::high_resolution_clock::now();
+    SimplicialCholesky<SpMat> chol(A.selfadjointView<Lower>());
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration<double>(end - start);
+    std::string factorization_time(std::to_string(elapsed.count()));
+
+    start = std::chrono::high_resolution_clock::now();
     VectorXd x = chol.solve(b);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = std::chrono::duration<double>(end - start);
+    std::string resolution_time(std::to_string(elapsed.count()));
 
     VectorXd tmp = x - xe;
     double rel_error = tmp.norm()/xe.norm();
 
-    std::cout << "Relative error: " << rel_error << std::endl;
+    // Writing logfile
 
+    std::string log_path = "../log/";
+    std::string platform = get_platform();
+    std::fstream logfile;
+    logfile.open(log_path + mat_name + "-" + "cpp-" + platform + ".log", std::fstream::app);
+
+    time_t curr_time;
+	tm * curr_tm;
+	char date_string[100];
+	
+	time(&curr_time);
+	curr_tm = localtime(&curr_time);
+	strftime(date_string, 50, "%d-%m-%Y %H:%M:%S", curr_tm);
+
+    if (logfile.is_open())
+    {
+        logfile << "Date, " << date_string << "\n";
+        logfile << "Factorization time, " << factorization_time << "\n";
+        logfile << "Resolution time, " << resolution_time << "\n";
+        logfile << "Relative error, " << rel_error << "\n\n";
+    }
+
+    logfile.close();
+
+    return 0;
 }
--- python/main.py
+++ python/main.py
@@ -1,20 +1,52 @@
+import sys
+import os
+import time
+import platform
+from datetime import datetime
+
 import numpy as np
+import scipy as sp
 import scipy.io as io
 from sksparse.cholmod import cholesky
 
-mat = io.loadmat("../res/matrices/ex15.mat")
+mat_name = sys.argv[1]
+if (not mat_name):
+    sys.exit(0)
 
-A = mat["Problem"]["A"][0][0]
+res_path = "../res/matrices/"
+mat_path = res_path + str(mat_name)
+
+mat = io.mmread(mat_path)
+
+A = sp.sparse.csc_matrix(mat)
 n = A.shape
 
 xe = np.ones(n[0])
 b = A.dot(xe)
 
 # Cholesky factorization
+factorization_time = time.process_time()
 factor = cholesky(A)
+factorization_time = time.process_time() - factorization_time
 
+resolution_time = time.process_time()
 x = factor.solve_A(b)
+resolution_time = time.process_time() - resolution_time
 
 rel_error = np.linalg.norm(x-xe)/np.linalg.norm(xe)
 
-print("Relative error: " + str(rel_error))
+# Writing logfile
+log_path = "../log/"
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
+
+platform = platform.system()
+logfile = open(log_path + mat_name + "-" + "python-" + platform + ".log", "a")
+date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+logfile.write("Date, " + date + "\n")
+logfile.write("Factorization time, " + str(factorization_time) + "\n")
+logfile.write("Resolution time, " + str(resolution_time) + "\n")
+logfile.write("Relative error, " + str(rel_error) + "\n\n")
+
+logfile.close()
